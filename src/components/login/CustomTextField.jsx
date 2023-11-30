987b6dd66a808997af9440ce16ee62f1b0078dad
ErickRRB <erickricardorb@gmail.com>
2023-11-30 16:21:58 -0300

    Arreglo bug de TExtField

--- src/components/login/CustomTextField.jsx
+++ src/components/login/CustomTextField.jsx
@@ -3,20 +3,18 @@ import FormHelperText from '@mui/material/FormHelperText';
 
 export const CustomTextField = ({ label, value, onChange, onFocus, onBlur, error, helperText, xsw }) => {
   return (
-    <div>
-      <TextField
-        sx={{ width: { xs: xsw ? xsw : '90%' }, margin:'0 0 2rem 0'}}
-        className='form_input'
-        id="filled-basic"
-        label={label}
-        variant="filled"
-        value={value}
-        onChange={onChange}
-        onFocus={onFocus}
-        onBlur={onBlur}
-        error={Boolean(error)}
-      />
-      <FormHelperText>{helperText}</FormHelperText>
-    </div>
-  );
-};
+    <TextField 
+                        sx={{ width: { xs: xsw ? xsw : '90%' }, margin:'0 0 2rem 0'}} 
+                        className='form_input' 
+                        id="filled-basic" 
+                        label={label} 
+                        variant="filled" 
+                        value={value}
+                        onChange={onChange} // Actualiza el estado local de email
+                        onFocus={onFocus}
+                        onBlur={onBlur}
+                        error={Boolean(error)}  // Agregado para manejar el estado de error
+                        helperText={<FormHelperText>{helperText}</FormHelperText>}  // Agregado para mostrar el mensaje de error
+                    />
+  )
+}
